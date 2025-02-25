63d6cc55269acf89e9ce77e446779b6ddaa107f0
IT21190148 <IT21190148@my.sliit.lk>
2025-02-25 12:45:01 +0530

    frontend some changes added

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
+=======
+
+>>>>>>> ede671f (frontend some changes added)
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
